f5dd9f979e7adcd731be02ba47f50d7807413637
AlexeiKurochkin <154975570+AlexeiKurochkin@users.noreply.github.com>
2025-06-12 18:26:38 +0300

    KS-1-1 Create branch Git

---
+++ src/main/kotlin/lesson1/task1.kt
@@ -0,0 +1,9 @@
+package org.example.lesson1
+
+fun main() {
+    val numberOfOrders = 75
+    val stringThankForPurchase = "Спасибо за покупку"
+
+    println("Количество заказов: $numberOfOrders")
+    println(stringThankForPurchase)
+}
